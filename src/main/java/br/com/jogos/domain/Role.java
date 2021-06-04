1ac4dc87281d73bfc43e84c98d50de68a2dae2b4
Douglas Franco <douglasmfranco@hotmail.com>
2021-06-04 16:18:09 -0300

    testando para heroku

--- src/main/java/br/com/jogos/domain/Role.java
+++ src/main/java/br/com/jogos/domain/Role.java
@@ -13,13 +13,16 @@ import lombok.Data;
 @Entity
 @Data
 public class Role implements GrantedAuthority{
+	
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String nome;
 	
-	@Override
+	
 	public String getAuthority() {
 		return nome;
 	}
