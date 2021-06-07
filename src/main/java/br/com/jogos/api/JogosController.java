c00a6e3eee48ee6f281a2e1411f4be7869c1c88c
Douglas Franco <douglasmfranco@hotmail.com>
2021-06-07 18:26:17 -0300

    arrumando conexao

--- src/main/java/br/com/jogos/api/JogosController.java
+++ src/main/java/br/com/jogos/api/JogosController.java
@@ -2,10 +2,8 @@ package br.com.jogos.api;
 
 import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.DeleteMapping;
