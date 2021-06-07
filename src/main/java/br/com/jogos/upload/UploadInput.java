880b75fa3e10e20f7a3e4909f779dff1470aefeb
Douglas Franco <douglasmfranco@hotmail.com>
2021-06-07 19:38:31 -0300

    atualizando dados - upload

---
+++ src/main/java/br/com/jogos/upload/UploadInput.java
@@ -0,0 +1,11 @@
+package br.com.jogos.upload;
+
+import lombok.Data;
+
+@Data
+public class UploadInput {
+private String fileName;
+private String base64;
+private String mimeType;
+	
+}
---
+++ src/main/java/br/com/jogos/upload/UploadOutput.java
@@ -0,0 +1,13 @@
+package br.com.jogos.upload;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UploadOutput {
+    private String url;
+}
